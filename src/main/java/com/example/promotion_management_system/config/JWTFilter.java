9fb654870460c8bf476f15d31af4c95f89c7f0f3
PularaPiyumali <120791863+PularaPiyumali@users.noreply.github.com>
2025-10-26 04:12:21 +0530

    Update AdminController

--- src/main/java/com/example/promotion_management_system/config/JWTFilter.java
+++ src/main/java/com/example/promotion_management_system/config/JWTFilter.java
@@ -53,7 +53,6 @@ public class JWTFilter extends OncePerRequestFilter {
                 SecurityContextHolder.getContext().setAuthentication(authToken);
             }
         }
-
         filterChain.doFilter(request, response);
     }
 }
